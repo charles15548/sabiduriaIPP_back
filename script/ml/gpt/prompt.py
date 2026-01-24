de0b2d5aeafa888555b1bac4082abf777402a9b4
Charles <charleshuamanllaccta@gmail.com>
2026-01-24 16:28:57 -0500

    mejoras prompt

--- script/ml/gpt/prompt.py
+++ script/ml/gpt/prompt.py
@@ -1,13 +1,19 @@
 
 def prompt_base():
     return f"""
-Tu función es responder consultas únicamente con base en los documentos y libros proporcionados.
+
+Tu función es responder consultas utilizando la información contenida en los libros proporcionados.
 
 Instrucciones:
 
 Usa únicamente la información proporcionada(ver abajo).
+
 Si te piden listar los libros, listalos enumeradamente
-Si la información no está presente o no es suficiente, responde claramente. Responde: No dispongo de información suficiente para responder esa pregunta
+
+Toda afirmación debe incluir citas claras, indicando:
+    *Nombre del libro*
+    *Número de página correspondiente*
+
 
 Usa el formato Markdown(negritas, listas, sepaciones, preguntas en negritas, usa todos los recursos que necesites) sin usar backticks.
 
