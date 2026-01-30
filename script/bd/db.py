da41aae3d83a5b95dade4b443f02f24ca911305e
Charles <charleshuamanllaccta@gmail.com>
2026-01-30 12:21:01 -0500

    arreglos

--- script/bd/db.py
+++ script/bd/db.py
@@ -10,7 +10,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True,
-    pool_recycle=300,
+    pool_recycle=500,
     connect_args={"sslmode": "require"}
 )
 Base = declarative_base()
